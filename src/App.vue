73e2464c1a75c3e9d8850cbb02ec4d6bf3ea742d
ChananthonTha <koonzee14@gmail.com>
2022-11-10 15:39:41 +0700

    up

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,13 @@
 <template>
-  <router-view></router-view>
+  <div class="bg-red-500 text-white p-4">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+  </div>
+  <RouterView />
 </template>
+
+<script>
+  export default {
+    name:"Vue3-Blog"  
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
+  <div>
     <div>
-        <h1>Home Page</h1>
+        <img src="https://picsum.photos/id/16/367/267" alt="img1">
     </div>
+    <div class="ml-2 p-2">
+      <div class="font-bold text-lg color-gold">กรุงเทพมหานคร</div>
+      <div class="text-sm">
+        กรุงเทพมหานคร เป็นเมืองหลวงและนครที่มีประชากรมากที่สุดของประเทศไทย
+        เป็นศูนย์กลางการปกครอง การศึกษา การคมนาคมขนส่ง การเงินการธนาคาร
+        การพาณิชย์ การสื่อสาร และความเจริญของประเทศ
+      </div>
+    </div>
+  </div>
 </template>
+
 <script>
 export default {
-    name:"HomePage",
-}
+  name: "HomePage",
+};
 </script>
-<style>
-
-</style>
+<style></style>
